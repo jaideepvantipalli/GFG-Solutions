e7ed082828b0df44f6d84fb471f5c9b31af3ff7d
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-01-26 20:50:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements before which no element is bigger/elements-before-which-no-element-is-bigger.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countElements(int[] arr) {
+        // code here
+        int max=arr[0],count=1;
+       for(int i=1;i<arr.length;i++){
+           if(arr[i]>max){
+               count++;
+               max=arr[i];
+           }
+       }
+       return count;
+    }
+}
