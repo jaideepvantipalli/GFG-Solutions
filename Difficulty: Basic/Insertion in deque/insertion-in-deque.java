329defe3932fb2267dcb2cd87fc842f636083b40
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-01-28 22:30:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Insertion in deque/insertion-in-deque.java
+++ Difficulty: Basic/Insertion in deque/insertion-in-deque.java
@@ -1,7 +1,10 @@
+// User function Template for Java
+
 class Solution {
-    public Deque<Integer> dqInsertion(List<Integer> arr) {
-        // code here
-        Deque<Integer> dq=new ArrayDeque<>();
+    // Function to insert all elements of the array in deque.
+    public static ArrayDeque<Integer> deque_Init(int arr[], int n) {
+        // Your code here
+        ArrayDeque<Integer> dq=new ArrayDeque<>();
         for(int i:arr) dq.add(i);
         return dq;
     }
