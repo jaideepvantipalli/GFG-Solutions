2b002ac413b25f386bd47cdac2f8cc02e2a2c92b
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-02-21 20:41:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        // code here
+        int n=citations.length;
+        int freq[]=new int[n+1];
+        for(int i=0;i<n;i++){
+            if(citations[i]>=n) freq[n]+=1;
+            else freq[citations[i]]+=1;
+        }
+        int h=n;
+        int s=freq[n];
+        while(s<h){
+            h--;
+            s+=freq[h];
+        }
+        return h;
+    }
+}
