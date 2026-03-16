e5b6f7d6fdba7430db232fb48f39781eef15b8fa
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-03-16 23:22:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue using Stack/queue-using-stack.java
@@ -0,0 +1,34 @@
+class myQueue {
+
+    // Initialize your data members
+    Stack<Integer> st1=new Stack<>();
+    Stack<Integer> st2=new Stack<>();
+
+    void enqueue(int x) {
+        // Implement enqueue operation
+        while(!st1.isEmpty()){
+            st2.push(st1.pop());
+        }
+        st1.push(x);
+        while(!st2.isEmpty()){
+            st1.push(st2.pop());
+        }
+    }
+
+    void dequeue() {
+        // Implement dequeue operation
+        if(st1.isEmpty()) return;
+        st1.pop();
+    }
+
+    int front() {
+        // Implement front operation
+        if(st1.isEmpty()) return -1;
+        return st1.peek();
+    }
+
+    int size() {
+        // Implement size operation
+        return st1.size();
+    }
+}
