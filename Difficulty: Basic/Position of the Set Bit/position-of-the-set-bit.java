8d9db4c3ea000d75da726ae5b11a7b02ad116afe
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-05-02 14:20:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Position of the Set Bit/position-of-the-set-bit.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPosition(int n) {
+        // code here
+        String s="";
+        int c=0,pos=0;
+        while(n > 0 && c < 2){
+            if(n%2 == 0)
+            s+="0";
+            else{
+                s+="1";
+                c++;
+            }
+            n/=2;
+        }
+        return c==1 ? s.indexOf('1')+1:-1;
+    }
+}
